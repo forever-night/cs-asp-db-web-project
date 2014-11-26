835546c52b4fa90204b54a981b9521913dac5232
forever-night <anna.upintheair@gmail.com>
2014-11-26 15:42:31 +0100

    phone insert form added

--- Database/MasterPage.Master.cs
+++ Database/MasterPage.Master.cs
@@ -61,14 +61,21 @@ namespace Database
 
         protected void btn_insert_emp_Click(object sender, EventArgs e)
         {
-            if (tb_emp_name.Text == String.Empty || tb_emp_surname.Text == String.Empty ||
-                tb_emp_personal_id.Text == String.Empty || dl_dept_id.SelectedValue == "empty")
+            if (this.tb_emp_name.Text == String.Empty || this.tb_emp_surname.Text == String.Empty ||
+                this.tb_emp_personal_id.Text == String.Empty || this.dl_dept_id.SelectedValue == "empty")
                 ChangeStatus(this.lbl_status_emp, Status.Warning);
-
+            else if (Regex.Match(this.tb_emp_personal_id.Text, @"^[0-9]+$").Success == false)
+            {
+                ChangeStatus(this.lbl_status_emp, Status.NoNumbers);
+                this.lbl_status_emp.Text += ": PESEL";
+            }
             else
             {
                 // insert data
 
+
+                // clear fields for later use
+                btn_clear_emp_Click(sender, e);
 
                 ChangeStatus(this.lbl_status_emp, Status.Insert);
             }
@@ -96,19 +103,49 @@ namespace Database
                 this.tb_street.Text == String.Empty || this.tb_house.Text == String.Empty ||
                 this.tb_zipcode.Text == String.Empty || this.dl_addr_emp_id.SelectedValue == "empty")
                 ChangeStatus(this.lbl_status_addr, Status.Warning);
-
             else if (Regex.Match(this.tb_zipcode.Text, @"^[0-9]+$").Success == false)
             {
                 ChangeStatus(this.lbl_status_addr, Status.NoNumbers);
                 this.lbl_status_addr.Text += ": Zip-Code";
             }
-
             else
             {
                 // insert data
 
 
+                btn_clear_addr_Click(sender, e);
+
                 ChangeStatus(this.lbl_status_addr, Status.Insert);
+            }
+        }
+
+        protected void btn_clear_tel_Click(object sender, EventArgs e)
+        {
+            this.dl_tel_emp_id.ClearSelection();
+
+            this.tb_tel_number.Text = String.Empty;
+
+
+            ChangeStatus(this.lbl_status_tel, Status.Clear);
+        }
+
+        protected void btn_insert_tel_Click(object sender, EventArgs e)
+        {
+            if (this.dl_tel_emp_id.SelectedValue == "empty" || this.tb_tel_number.Text == String.Empty)
+                ChangeStatus(this.lbl_status_tel, Status.Warning);
+            else if (Regex.Match(this.tb_tel_number.Text, @"^[0-9]+$").Success == false)
+            {
+                ChangeStatus(this.lbl_status_tel, Status.NoNumbers);
+                this.lbl_status_tel.Text += ": Phone";
+            }
+            else
+            {
+                // insert data
+
+
+                btn_clear_tel_Click(sender, e);
+
+                ChangeStatus(this.lbl_status_tel, Status.Insert);
             }
         }
     }
