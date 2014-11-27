73cd985c13b8ad60187a1678fc66a44b808f6abd
forever-night <anna.upintheair@gmail.com>
2014-11-27 12:38:14 +0100

    updated sql statements

---
+++ Database/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Database
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            // Session["User"] = "";
+            // Session["Role"] = -1;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            // set user & role back to values they had before login
+            // Session["User"] = "";
+            // Session["Role"] = -1;
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Database/Tables.aspx.cs
+++ Database/Tables.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Data.SqlClient;
 using System.Configuration;
@@ -16,15 +17,47 @@ namespace Database
         string txtSqlConnect = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
         string txtSqlCommand;
         SqlCommand sqlCommand;
-
+ 
 
         protected enum Status { Clear, Insert, Warning, NoNumbers };
+
+
+        protected enum Forms { Employees, Addresses, Telephones };
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }       
+        }
+
+
+        protected void ClearForm(Forms form)
+        {
+            if (form == Forms.Employees)
+            {
+                this.dl_dept_id.ClearSelection();
+
+                this.tb_emp_name.Text = String.Empty;
+                this.tb_emp_surname.Text = String.Empty;
+                this.tb_emp_personal_id.Text = String.Empty;
+            }
+            else if (form == Forms.Addresses)
+            {
+                this.dl_addr_emp_id.ClearSelection();
+
+                this.tb_country.Text = String.Empty;
+                this.tb_city.Text = String.Empty;
+                this.tb_street.Text = String.Empty;
+                this.tb_house.Text = String.Empty;
+                this.tb_zipcode.Text = String.Empty;
+            }
+            else if (form == Forms.Telephones)
+            {
+                this.dl_tel_emp_id.ClearSelection();
+
+                this.tb_tel_number.Text = String.Empty;
+            }
+        }
 
 
         protected void ChangeStatus(Label status, Status st)
@@ -54,13 +87,7 @@ namespace Database
 
         protected void btn_clear_emp_Click(object sender, EventArgs e)
         {
-            this.tb_emp_name.Text = String.Empty;
-            this.tb_emp_surname.Text = String.Empty;
-            this.tb_emp_personal_id.Text = String.Empty;
-
-
-            this.dl_dept_id.ClearSelection();
-
+            ClearForm(Forms.Employees);
 
             ChangeStatus(this.lbl_status_emp, Status.Clear);
         }
@@ -80,24 +107,26 @@ namespace Database
             {
                 using (SqlConnection sqlConnect = new SqlConnection(txtSqlConnect))
                 {
-                    txtSqlCommand = "INSERT INTO Employees (emp_name, emp_surname, emp_personal_id, emp_dept_id) VALUES (@emp_name, @emp_surname, @emp_personal_id, @emp_dept_id)";
+                    txtSqlCommand = "INSERT INTO Employees (emp_name, emp_surname, emp_personal_id, emp_dept_id) VALUES (@emp_name, @emp_surname, @emp_personal_id, @emp_dept_id);";
+
 
                     sqlCommand = new SqlCommand(txtSqlCommand, sqlConnect);
+
 
                     sqlCommand.Parameters.AddWithValue("@emp_name", this.tb_emp_name.Text);
                     sqlCommand.Parameters.AddWithValue("@emp_surname", this.tb_emp_surname.Text);
                     sqlCommand.Parameters.AddWithValue("@emp_personal_id", this.tb_emp_personal_id.Text);
                     sqlCommand.Parameters.AddWithValue("@emp_dept_id", this.dl_dept_id.Text);
 
+
                     sqlCommand.Connection.Open();
                     sqlCommand.ExecuteNonQuery();
                 }
 
-                // TODO reload page, clear query, update and show updated DB
+                // TODO show updated DB
 
 
-                // clear form, display status
-                btn_clear_emp_Click(sender, e);
+                ClearForm(Forms.Employees);
 
                 ChangeStatus(this.lbl_status_emp, Status.Insert);
             }
@@ -106,14 +135,7 @@ namespace Database
 
         protected void btn_clear_addr_Click(object sender, EventArgs e)
         {
-            this.dl_addr_emp_id.ClearSelection();
-
-            this.tb_country.Text = String.Empty;
-            this.tb_city.Text = String.Empty;
-            this.tb_street.Text = String.Empty;
-            this.tb_house.Text = String.Empty;
-            this.tb_zipcode.Text = String.Empty;
-
+            ClearForm(Forms.Addresses);
 
             ChangeStatus(this.lbl_status_addr, Status.Clear);
         }
@@ -132,31 +154,30 @@ namespace Database
             }
             else
             {
-                // insert data
                 using (SqlConnection sqlConnect = new SqlConnection(txtSqlConnect))
                 {
-                    // TODO select Employee where emp_id == addr_emp_id and insert emp_addr_id = addr_id
+                    txtSqlCommand = "INSERT INTO Addresses (addr_emp_id, addr_country, addr_city, addr_street, addr_house, addr_zipcode) VALUES (@addr_emp_id, @addr_country, @addr_city, @addr_street, @addr_house, @addr_zipcode); UPDATE Employees SET emp_addr_id = addr_id FROM Addresses WHERE emp_id=@addr_emp_id;";
 
-                    txtSqlCommand = "INSERT INTO Addresses (addr_emp_id, addr_country, addr_city, addr_street, addr_house, addr_zipcode) VALUES (@addr_emp_id, @addr_country, @addr_city, @addr_street, @addr_house, @addr_zipcode)";
 
                     sqlCommand = new SqlCommand(txtSqlCommand, sqlConnect);
 
-                    sqlCommand.Parameters.AddWithValue("@addr_emp_id", this.dl_addr_emp_id.Text);
+
+                    sqlCommand.Parameters.AddWithValue("@addr_emp_id", Int32.Parse(this.dl_addr_emp_id.Text));
                     sqlCommand.Parameters.AddWithValue("@addr_country", this.tb_country.Text);
                     sqlCommand.Parameters.AddWithValue("@addr_city", this.tb_city.Text);
                     sqlCommand.Parameters.AddWithValue("@addr_street", this.tb_street.Text);
-                    sqlCommand.Parameters.AddWithValue("@addr_house", this.tb_house.Text);
-                    sqlCommand.Parameters.AddWithValue("@addr_zipcode", this.tb_zipcode.Text);
+                    sqlCommand.Parameters.AddWithValue("@addr_house", Int32.Parse(this.tb_house.Text));
+                    sqlCommand.Parameters.AddWithValue("@addr_zipcode", Int32.Parse(this.tb_zipcode.Text));
+
 
                     sqlCommand.Connection.Open();
                     sqlCommand.ExecuteNonQuery();
                 }
 
-                // TODO reload page, clear query, update and show updated DB
+                // TODO update and show updated DB
 
 
-                // clear form, display status
-                btn_clear_addr_Click(sender, e);
+                ClearForm(Forms.Addresses);
 
                 ChangeStatus(this.lbl_status_addr, Status.Insert);
             }
@@ -164,10 +185,7 @@ namespace Database
 
         protected void btn_clear_tel_Click(object sender, EventArgs e)
         {
-            this.dl_tel_emp_id.ClearSelection();
-
-            this.tb_tel_number.Text = String.Empty;
-
+            
 
             ChangeStatus(this.lbl_status_tel, Status.Clear);
         }
@@ -183,24 +201,26 @@ namespace Database
             }
             else
             {
-                // insert data
                 using (SqlConnection sqlConnect = new SqlConnection(txtSqlConnect))
                 {
-                    // TODO select Employee where emp_id == tel_emp_id and insert emp_tel_id == tel_id
+                    txtSqlCommand = "INSERT INTO Telephones (tel_emp_id, tel_num) VALUES (@tel_emp_id, @tel_num); UPDATE Employees SET emp_tel_id = tel_id FROM Telephones WHERE emp_id=@tel_emp_id;";
 
-                    txtSqlCommand = "INSERT INTO Telephones (tel_emp_id, tel_num) VALUES (@tel_emp_id, @tel_num);";
 
                     sqlCommand = new SqlCommand(txtSqlCommand, sqlConnect);
+
+
+                    sqlCommand.Parameters.AddWithValue("@tel_emp_id", Int32.Parse(this.dl_tel_emp_id.Text));
+                    sqlCommand.Parameters.AddWithValue("@tel_num", Int32.Parse(this.tb_tel_number.Text));
+
 
                     sqlCommand.Connection.Open();
                     sqlCommand.ExecuteNonQuery();
                 }
 
-                // TODO reload page, clear query, update and show updated DB
+                // TODO update and show updated DB
 
 
-                // clear form, display status
-                btn_clear_tel_Click(sender, e);
+                ClearForm(Forms.Telephones);
 
                 ChangeStatus(this.lbl_status_tel, Status.Insert);
             }
